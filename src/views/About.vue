6a7d18270e50c71656cf7a72842809b2f34ff28b
zmj0920 <mj960920@163.com>
2020-09-22 00:06:13 +0800

    tsx

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<script lang="tsx">
+import { defineComponent } from 'vue'
+
+const wrapper = defineComponent({
+  setup() {
+    return () => (
+      <div>
+      	一段文字
+      	<h1 class="ok">
+      		标题
+      	</h1>
+      </div>
+    )
+  },
+})
+export default wrapper
+</script>
